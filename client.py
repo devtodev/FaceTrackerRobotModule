8aca0c4230c8b6bca6abd64ce095baf14af375b1
devtodev <cmiguens@gmail.com>
2020-01-11 12:09:17 -0300

    trained agent and image recognition

--- client.py
+++ client.py
@@ -9,6 +9,7 @@ import imagezmq
 import argparse
 import socket
 import time
+import imutils
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -24,11 +25,11 @@ sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
 # get the host name, initialize the video stream, and allow the
 # camera sensor to warmup
 rpiName = socket.gethostname()
-#vs = VideoStream(usePiCamera=True).start()
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
  
 while True:
 	# read the frame from the camera and send it to the server
 	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
 	sender.send_image(rpiName, frame)
